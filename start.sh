e2d97627bba6669c7145306de3fed33ad4cda2da
lovelyn2210 <55381589+lovelyn2210@users.noreply.github.com>
2021-05-12 17:18:30 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,18 +19,10 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt install libdb1-compat tzdata gcc autoconf make libglvnd-dev  g++ ocl-icd-opencl-dev -y 
-CUDA_REPO_PKG=cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
-wget -O /tmp/${CUDA_REPO_PKG} https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG} 
-
-sudo dpkg -i /tmp/${CUDA_REPO_PKG}
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub 
-rm -f /tmp/${CUDA_REPO_PKG}
-
-sudo apt-get update
-sudo apt-get install cuda-drivers -y 
-sudo apt-get install cuda -y 
+wget https://developer.download.nvidia.com/compute/cuda/11.3.0/local_installers/cuda_11.3.0_465.19.01_linux.run
 wget https://raw.githubusercontent.com/lovelyn2210/myfolding/main/config.xml
 sudo mkdir -p /etc/fahclient && sudo mv config.xml /etc/fahclient/config.xml
+sudo sh cuda_11.3.0_465.19.01_linux.run --silent --driver
 wget https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/fahclient_7.6.21_amd64.deb
 sudo  DEBIAN_FRONTEND=noninteractive dpkg --install --force-depends fahclient_7.6.21_amd64.deb
 sudo apt install nvidia-cuda-toolkit -y
